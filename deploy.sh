5f13ff0833fb0d26c6bcc8fdd32138023776fa2f
c776188 <tokisaki776188@gmail.com>
2019-12-18 10:40:53 +0800

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,13 @@
 set -e
 # 打包
 npm run build
-# 移動至到打包後的dist目錄 
-cd dist
-git init //因為dist資料夾預設是被ignore的，因此在進入dist資料夾後初始化git
-git add -A
+
+git branch -D gh-pages
+git push origin :gh-pages
+
+git checkout -b gh-pages
+git add -f dist
 git commit -m 'deploy'
-# 部署到 https://github.com/chou0728/eric-project.git 分支為 gh-pages
-git subtree push --prefix -f dist https://github.com/c776188/ProfileVue.git master:gh-pages
-//將dist資料夾中的內容推送至遠端eric-project的gh-pages分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
-cd -
+git subtree push --prefix dist origin gh-pages
+
+git checkout master
