a343ba4bd680c46e1ae9882e1ad01d28b4df75ad
林峻毅 <lin@linjunyide-MacBook-Air.local>
2019-12-18 21:17:28 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -30,6 +30,8 @@ Vue.use(Router)
 
 // Create a new router
 const router = new Router({
+    base: 'ProfileVue',
+    mode: 'history',
     routes: paths.map(path => route(path.path, path.view, path.name)).concat([
         { path: '/ProfileVue/*', redirect: '/' }
     ]),
