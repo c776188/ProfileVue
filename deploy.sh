e9a0bd3247e078d4ce328b0782f816248bb52634
c776188 <tokisaki776188@gmail.com>
2019-12-19 09:31:05 +0800

    command # & update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
-//deploy.sh
+# //deploy.sh
 #!/usr/bin/env sh
 # 當發生錯誤時終止腳本運行
 set -e
 # 打包
 npm run build
 
-git branch -D gh-pages
-git push origin :gh-pages
+git branch -D master
+git push origin :master
 
-git checkout master
+git checkout -b master
 git add -f dist
 git commit -m 'deploy'
 git subtree push --prefix dist origin master
