219717500cf5b6f4ea1b032b320a9537a4c4e707
林峻毅 <lin@linjunyide-MacBook-Air.local>
2019-12-22 23:18:17 +0800

    skill table

--- src/views/TableList.vue
+++ src/views/TableList.vue
@@ -1,50 +1,25 @@
 <template>
-  <v-container
-    fill-height
-    fluid
-    grid-list-xl
-  >
-    <v-layout
-      justify-center
-      wrap
-    >
-      <v-flex
-        md12
-      >
-        <material-card
-          color="green"
-          title="Simple Table"
-          text="Here is a subtitle for this table"
-        >
-          <v-data-table
-            :headers="headers"
-            :items="items"
-            hide-actions
-          >
-            <template
-              slot="headerCell"
-              slot-scope="{ header }"
-            >
+  <v-container fill-height fluid grid-list-xl>
+    <v-layout justify-center wrap>
+      <v-flex md12>
+        <material-card color="green" title="Skill" text="Here is a subtitle for this table">
+          <v-data-table :headers="headers" :items="items" hide-actions>
+            <template slot="headerCell" slot-scope="{ header }">
               <span
                 class="subheading font-weight-light text-success text--darken-3"
                 v-text="header.text"
               />
             </template>
-            <template
-              slot="items"
-              slot-scope="{ item }"
-            >
+            <template slot="items" slot-scope="{ item }">
               <td>{{ item.name }}</td>
-              <td>{{ item.country }}</td>
-              <td>{{ item.city }}</td>
-              <td class="text-xs-right">{{ item.salary }}</td>
+              <td>{{ item.proficiency }}</td>
+              <td>{{ item.experience }}</td>
+              <!-- <td class="text-xs-right">{{ item.salary }}</td> -->
             </template>
           </v-data-table>
         </material-card>
       </v-flex>
-      <v-flex
-        md12
-      >
+      <v-flex md12>
         <material-card
           color="green"
           flat
@@ -52,28 +27,15 @@
           title="Table on Plain Background"
           text="Here is a subtitle for this table"
         >
-          <v-data-table
-            :headers="headers"
-            :items="items.slice(0, 7)"
-            hide-actions
-          >
-            <template
-              slot="headerCell"
-              slot-scope="{ header }"
-            >
-              <span
-                class="subheading font-weight-light text--darken-3"
-                v-text="header.text"
-              />
+          <v-data-table :headers="headers" :items="items.slice(0, 7)" hide-actions>
+            <template slot="headerCell" slot-scope="{ header }">
+              <span class="subheading font-weight-light text--darken-3" v-text="header.text" />
             </template>
-            <template
-              slot="items"
-              slot-scope="{ item }"
-            >
+            <template slot="items" slot-scope="{ item }">
               <td>{{ item.name }}</td>
-              <td>{{ item.country }}</td>
-              <td>{{ item.city }}</td>
-              <td class="text-xs-right">{{ item.salary }}</td>
+              <td>{{ item.proficiency }}</td>
+              <td>{{ item.experience }}</td>
+              <!-- <td class="text-xs-right">{{ item.salary }}</td> -->
             </template>
           </v-data-table>
         </material-card>
@@ -88,60 +50,64 @@ export default {
     headers: [
       {
         sortable: false,
-        text: 'Name',
-        value: 'name'
+        text: "Skill Name",
+        value: "name"
       },
       {
         sortable: false,
-        text: 'Country',
-        value: 'country'
+        text: "Proficiency",
+        value: "proficiency"
       },
       {
-        sortable: false,
-        text: 'City',
-        value: 'city'
-      },
-      {
-        sortable: false,
-        text: 'Salary',
-        value: 'salary',
-        align: 'right'
+        sortable: true,
+        text: "Experience",
+        value: "experience"
       }
+      // {
+      //   sortable: false,
+      //   text: "Salary",
+      //   value: "salary",
+      //   align: "right"
+      // }
     ],
     items: [
       {
-        name: 'Dakota Rice',
-        country: 'Niger',
-        city: 'Oud-Tunrhout',
-        salary: '$35,738'
+        name: "JAVA",
+        proficiency: "Niger",
+        experience: "5"
+        // salary: "$35,738"
       },
       {
-        name: 'Minerva Hooper',
-        country: 'Curaçao',
-        city: 'Sinaai-Waas',
-        salary: '$23,738'
-      }, {
-        name: 'Sage Rodriguez',
-        country: 'Netherlands',
-        city: 'Overland Park',
-        salary: '$56,142'
-      }, {
-        name: 'Philip Chanley',
-        country: 'Korea, South',
-        city: 'Gloucester',
-        salary: '$38,735'
-      }, {
-        name: 'Doris Greene',
-        country: 'Malawi',
-        city: 'Feldkirchen in Kārnten',
-        salary: '$63,542'
-      }, {
-        name: 'Mason Porter',
-        country: 'Chile',
-        city: 'Gloucester',
-        salary: '$78,615'
+        name: "PHP",
+        proficiency: "Curaçao",
+        experience: "3"
+        // salary: "$23,738"
+      },
+      {
+        name: "C",
+        proficiency: "Netherlands",
+        experience: "3"
+        // salary: "$56,142"
+      },
+      {
+        name: "Golang",
+        proficiency: "Korea, South",
+        experience: "1"
+        // salary: "$38,735"
       }
+      // {
+      //   name: "Doris Greene",
+      //   proficiency: "Malawi",
+      //   experience: "Feldkirchen in Kārnten",
+      //   // salary: "$63,542"
+      // },
+      // {
+      //   name: "Mason Porter",
+      //   proficiency: "Chile",
+      //   experience: "Gloucester",
+      //   // salary: "$78,615"
+      // }
     ]
   })
-}
+};
 </script>
