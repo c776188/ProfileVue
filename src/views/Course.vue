7907b8f14a96b0b643909ff4bf510fd3950d6125
林峻毅 <lin@linjunyide-MacBook-Air.local>
2020-01-06 22:46:34 +0800

    course templ

--- src/views/Course.vue
+++ src/views/Course.vue
@@ -1,56 +1,110 @@
-
+<style>
+body {
+  background: #111;
+  font-size: 18px;
+  color: #fff;
+}
+h1 {
+  font-size: 2em;
+  text-align: center;
+  margin: 20px 0 40px;
+  padding: 0;
+}
+.container {
+  background: #222;
+  width: 80%;
+  margin: 0 auto;
+  overflow: auto;
+  padding: 2%;
+}
+.right-content,
+.left-content {
+  float: left;
+  display: block;
+  width: 40%;
+  padding: 1% 2%;
+}
+.left-content {
+  border-right: 5px dotted #666;
+  text-align: right;
+}
+.left-content p {
+  margin: 150px 0 0;
+}
+.right-content p {
+  margin: 80px 0 150px;
+}
+.container p {
+  -webkit-transition: all 0.7s ease-in-out;
+  -moz-transition: all 0.7 ease-in-out;
+  -ms-transition: all 0.7s ease-in-out;
+  -o-transition: all 0.7s ease-in-out;
+  transition: all 0.7s ease-in-out;
+}
+.left-content .more-padding {
+  padding-right: 100px;
+}
+.right-content .more-padding {
+  padding-left: 100px;
+}
+</style>
 <template>
-  <v-data-table
-    class="fb-table-elem"
-    :headers="headers"
-    :items="items"
-    hide-actions
-    item-key="name"
-    expand
-  >
-    <template slot="items" slot-scope="props">
-      <tr @click="props.expanded = !props.expanded">
-        <td>
-          <div class="datatable-cell-wrapper">{{ props.item.name }}</div>
-        </td>
-        <td class="text-xs-right">
-          <div class="datatable-cell-wrapper">{{ props.item.calories }}</div>
-        </td>
-        <td class="text-xs-right">
-          <div class="datatable-cell-wrapper">{{ props.item.fat }}</div>
-        </td>
-        <td class="text-xs-right">
-          <div class="datatable-cell-wrapper">{{ props.item.carbs }}</div>
-        </td>
-        <td class="text-xs-right">
-          <div class="datatable-cell-wrapper">{{ props.item.protein }}</div>
-        </td>
-        <td class="text-xs-right">
-          <div class="datatable-cell-wrapper">{{ props.item.sodium }}</div>
-        </td>
-        <td class="text-xs-right">
-          <div class="datatable-cell-wrapper">{{ props.item.calcium }}</div>
-        </td>
-        <td class="text-xs-right">
-          <div class="datatable-cell-wrapper">{{ props.item.iron }}</div>
-        </td>
-      </tr>
-    </template>
-    <template slot="expand" slot-scope="props">
-      <v-card flat>
-        <v-card-text>Peek-a-boo! {{ props.item.name }}</v-card-text>
-        <v-card-text>Peek-a-boo!</v-card-text>
-        <div class="datatable-container"></div>
-      </v-card>
-    </template>
-  </v-data-table>
+  <div class="container">
+    <h1>Simple Animated Timeline Scroller</h1>
+    <div class="left-content">
+      <p>Content 01</p>
+      <p>Content 02</p>
+      <p>Content 03</p>
+      <p>Content 04</p>
+      <p>Content 05</p>
+      <p>Content 06</p>
+      <p>Content 07</p>
+      <p>Content 08</p>
+      <p>Content 09</p>
+      <p>Content 10</p>
+    </div>
+    <div class="right-content">
+      <p>Content 01</p>
+      <p>Content 02</p>
+      <p>Content 03</p>
+      <p>Content 04</p>
+      <p>Content 05</p>
+      <p>Content 06</p>
+      <p>Content 07</p>
+      <p>Content 08</p>
+      <p>Content 09</p>
+      <p>Content 10</p>
+    </div>
+  </div>
 </template>
+
 <script>
+import jQuery from "jquery";
+let $ = jQuery;
+
 export default {
   methods: {
     clicked(value) {
       this.expanded.push(value);
     }
+  },
+  mounted: function() {
+    $(window).scroll(function() {
+      $(".container p").each(function() {
+        var scrollTop = $(window).scrollTop(),
+          elementOffset = $(this).offset().top,
+          distance = elementOffset - scrollTop,
+          windowHeight = $(window).height(),
+          breakPoint = windowHeight * 0.9;
+
+        if (distance > breakPoint) {
+          $(this).addClass("more-padding");
+        }
+        if (distance < breakPoint) {
+          $(this).removeClass("more-padding");
+        }
+      });
+    });
   },
   data() {
     return {
